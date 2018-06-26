cc4f0ee2304112531b86445041e47cf5ca4bbd36
sunsetxh <wyc984872769@hotmail.com>
2018-06-27 03:03:21 +0800

    ct

---
+++ Apo_Class3D_job005_ct.sh
@@ -0,0 +1,10 @@
+#use Refine3D job001
+#use two model to find the result
+
+cd /home/ycwang/workdir/20180110_data/Apo_data_snf2
+
+DATE=$(date +%Y%m%d%H%M)
+
+nohup relion_refine --o Class3D/job005/run --continue Class3D/job005/run_it001_optimiser.star --firstiter_cc --ini_high 2 --dont_combine_weights_via_disc --pool 3 --ctf --iter 2 --tau2_fudge 4 --particle_diameter 200 --K 2 --flatten_solvent --zero_mask --oversampling 1 --healpix_order 5 --sigma_ang 3.33333 --offset_range 5 --offset_step 2 --sym C1 --norm --scale  --j 4 --gpu "6,7" > Class3D_job005_ct_${DATE}.out &
+
+
