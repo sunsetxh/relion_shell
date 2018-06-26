d78777cbe2a230a31e63588b35daa408b0eae792
sunsetxh <wyc984872769@hotmail.com>
2018-06-27 00:50:05 +0800

    同样对两个数据集二分类

---
+++ ATP_Class3D_job005.sh
@@ -0,0 +1,10 @@
+#use Refine3D job002
+#use two model to find the result
+
+cd /home/ycwang/workdir/20180110_data/ATP_data_snf2
+
+DATE=$(date +%Y%m%d%H%M)
+
+nohup relion_refine --o Class3D/job005/run --i Refine3D/job002/run_data.star --ref clus1-160.mrc --firstiter_cc --ini_high 2 --dont_combine_weights_via_disc --pool 3 --ctf --iter 1 --tau2_fudge 4 --particle_diameter 200 --K 2 --flatten_solvent --zero_mask --oversampling 1 --healpix_order 5 --sigma_ang 3.33333 --offset_range 5 --offset_step 2 --sym C1 --norm --scale  --j 4 --gpu "4,5" > Class3D_job005_${DATE}.out &
+
+
